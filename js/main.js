4d2b0fb61ce550948b801e86bb6a35af409b1886
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2018-04-03 15:12:09 +0200

    fix the img and made scroll for window with js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+  $(".nav li a").click(function(e) {
+    console.log($(this).data('scroll'));
+    $("html, body").animate({
+      scrollTop: $($(this).data('scroll')).offset().top
+    }, 1000)
+    e.preventDefault();
 
-
-  $("h1").css("color", "red")
-
-
+  });
 });
