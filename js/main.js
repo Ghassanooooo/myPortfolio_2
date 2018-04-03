d5b00ccd2d3e29d57020659e2f6832abc121873c
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2018-04-03 15:55:23 +0200

    fix the nav opacity by js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(".navbar-default").offset().top > 100) {
+      $(".navbar-fixed-top").addClass('navScroll');
+    } else {
+      $(".navbar-fixed-top").removeClass('navScroll');
+    }
+  });
+
   $(".nav li a").click(function(e) {
     console.log($(this).data('scroll'));
     $("html, body").animate({
       scrollTop: $($(this).data('scroll')).offset().top
-    }, 1000)
+    }, 800)
     e.preventDefault();
 
   });
